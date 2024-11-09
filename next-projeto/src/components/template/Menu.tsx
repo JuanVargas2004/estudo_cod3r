ff028c0f905a5b02d00aaa5dc5a51fba48230b55
Juan Vargas <juansvc2004@gmail.com>
2024-11-09 00:20:04 -0300

    Adicionando CSS Global para Tailwind e configurando Menu

--- next-projeto/src/components/template/Menu.tsx
+++ next-projeto/src/components/template/Menu.tsx
@@ -1,15 +1,15 @@
 import MenuItem from "./MenuItem";
-import { IconHome, IconNumber1, IconNumber2, IconForms, IconLayout} from "@tabler/icons-react";
+import { IconHome, IconGhost, IconBarcode, IconForms, IconLayout} from "@tabler/icons-react";
 
 export default function Menu(){
     return (
         <aside className="bg-[#102E55] w-80 p-6 flex justify-center items-center">
             <nav className="flex flex-col gap-5">
                 <MenuItem texto="Início" href="/" icone={<IconHome/>}></MenuItem>
-                <MenuItem texto="Primeiro Componente" href="/primeiro" icone={<IconNumber1/>}></MenuItem>
-                <MenuItem texto="Componente UseState" href="/contador" icone={<IconNumber2/>}></MenuItem>
-                <MenuItem texto="Formulário" href="/formulario" icone={<IconForms/>}></MenuItem>
+                <MenuItem texto="Página Não Existe" href="/primeiro" icone={<IconGhost/>}></MenuItem>
                 <MenuItem texto="Layout" href="/layout" icone={<IconLayout></IconLayout>}/>
+                <MenuItem texto="Produtos" href="/contador" icone={<IconBarcode/>}></MenuItem>
+                <MenuItem texto="Formulário" href="/formulario" icone={<IconForms/>}></MenuItem>
             </nav>
         </aside>
     );
